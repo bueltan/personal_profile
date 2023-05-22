63067aa5b528f3fcff4fbafd696a4a9fe50e17bc
bueltan <denisbueltan@gmail.com>
2023-05-22 00:23:07 +0000

    add function for notification activity

--- lib/controllers/page_controller.dart
+++ lib/controllers/page_controller.dart
@@ -1,38 +1,75 @@
-
 import 'package:denis_profile/controllers/atom_controller.dart';
 import 'package:denis_profile/models/item_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:dart_ipify/dart_ipify.dart';
+import 'dart:io' show Platform;
+import 'send_message/sent_messages.dart';
 
-class PageStateController extends GetxController  {
+class PageStateController extends GetxController {
   bool expanded = false;
   final AtomController atomController = Get.find<AtomController>();
-  
+
   PageController pageController = PageController(initialPage: 0);
   int currentPageIndex = 0;
+  String? ipv4;
+  String? hostname;
 
-  void jumpToPage({PageItem ? pageItem, Duration distanceTime = const Duration(milliseconds: 500), animate = true}){
-    if (animate){
-    pageController.animateToPage(pageItem?.indexPage??0, duration:  distanceTime, curve: Curves.ease);
+  @override
+  void onInit() async {
+    try {
+      hostname = Platform.localHostname;
+    } catch (e) {
+      print(e);
+    }
+    try {
+      ipv4 = await Ipify.ipv4();
+    } catch (e) {
+      print(e);
+    }
 
-    }else{
-      pageController.jumpToPage(pageItem?.indexPage??0);
+    try {
+      String message = "new visitor";
+      await SentMessageController().sendMessage(message);
+    } catch (e) {
+      print(e);
+    }
+
+    super.onInit();
+  }
+
+  void jumpToPage(
+      {PageItem? pageItem,
+      Duration distanceTime = const Duration(milliseconds: 500),
+      animate = true}) async {
+    try {
+      String message = "change page to ${pageItem?.keyName} ";
+      await SentMessageController().sendMessage(message);
+    } catch (e) {
+      print(e);
+    }
+    if (animate) {
+      pageController.animateToPage(pageItem?.indexPage ?? 0,
+          duration: distanceTime, curve: Curves.ease);
+    } else {
+      pageController.jumpToPage(pageItem?.indexPage ?? 0);
     }
   }
-  void expandPage(){
-      expanded = true;
-      atomController.setExpand();
-      update(["PageState"]);
+
+  void expandPage() {
+    expanded = true;
+    atomController.setExpand();
+    update(["PageState"]);
   }
-  void shrinkPage(){
-      expanded = false;
-      atomController.setShrink();
-      update(["PageState"]);
+
+  void shrinkPage() {
+    expanded = false;
+    atomController.setShrink();
+    update(["PageState"]);
   }
-  void changeIndexPage(int index){
+
+  void changeIndexPage(int index) {
     currentPageIndex = index;
     update(["pageIndex"]);
   }
-
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:denis_profile/controllers/atom_controller.dart';
 import 'package:denis_profile/controllers/projects_controller.dart';
 import 'package:denis_profile/pages/home.dart';
-import 'package:denis_profile/pages/page_projects.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'components/app_drawer.dart';
 import 'controllers/chart_controller.dart';
 import 'components/translation.dart';
 import 'controllers/page_controller.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,24 +23,20 @@ class MyApp extends StatelessWidget {
     Get.put(ChartControl());
     Get.put(CaruselProjectsControl());
 
-    return Builder(
-      builder: (context) {
-        return GetMaterialApp(
-          translations: Dictionary(),
-          locale: Get.deviceLocale,
-          fallbackLocale: const Locale('en', 'US'),
-          title: 'Denis Germán Giménez',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home:  const AppDrawer(
-            child: Home(),
-          ),
-          
-        );
-      }
-    );
+    return Builder(builder: (context) {
+      return GetMaterialApp(
+        translations: Dictionary(),
+        locale: Get.deviceLocale,
+        fallbackLocale: const Locale('en', 'US'),
+        title: 'Denis Germán Giménez',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const AppDrawer(
+          child: Home(),
+        ),
+      );
+    });
   }
 }
-
