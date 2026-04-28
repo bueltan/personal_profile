9f2060ed1b3f24644799e05727bc00e6d9333e51
Bueltan <denisbueltan@gmail.com>
2026-04-28 02:53:43 -0300

    add sitmap

--- lib/controllers/page_controller.dart
+++ lib/controllers/page_controller.dart
@@ -1,7 +1,21 @@
 import 'package:denis_profile/controllers/atom_controller.dart';
 import 'package:denis_profile/models/item_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:js_interop';
+
+const Map<int, String> _indexToPath = {
+  0: '/',
+  1: '/about',
+  2: '/knowledge',
+  3: '/experience',
+  4: '/projects',
+  5: '/contact',
+};
+
+@JS('history.pushState')
+external void _pushState(JSAny? data, String title, String url);
 
 class PageStateController extends GetxController {
   bool expanded = true;
@@ -11,6 +25,22 @@ class PageStateController extends GetxController {
   final PageController pageController = PageController(initialPage: 0);
 
   int currentPageIndex = 0;
+
+  @override
+  void onInit() {
+    super.onInit();
+    if (kIsWeb) {
+      pageController.addListener(_syncUrl);
+    }
+  }
+
+  void _syncUrl() {
+    final int page = pageController.page?.round() ?? 0;
+    final String path = _indexToPath[page] ?? '/';
+    try {
+      _pushState(null, '', path);
+    } catch (_) {}
+  }
 
   void jumpToPage({
     PageItem? pageItem,
@@ -49,6 +79,7 @@ class PageStateController extends GetxController {
 
   @override
   void onClose() {
+    pageController.removeListener(_syncUrl);
     pageController.dispose();
     super.onClose();
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,59 @@
+
 import 'package:denis_profile/controllers/atom_controller.dart';
 import 'package:denis_profile/controllers/projects_controller.dart';
+import 'package:denis_profile/models/item_page.dart';
 import 'package:denis_profile/pages/home.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'components/app_drawer.dart';
 import 'controllers/chart_controller.dart';
 import 'components/translation.dart';
 import 'controllers/page_controller.dart';
-
+import 'dart:js_interop';
+ 
+@JS('getInitialPage')
+external JSNumber? _getInitialPage();
+ 
+int _readInitialPage() {
+  if (!kIsWeb) return 0;
+  try {
+    final JSNumber? result = _getInitialPage();
+    return result?.toDartInt ?? 0;
+  } catch (_) {
+    return 0;
+  }
+}
+ 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
   runApp(const MyApp());
 }
-
-
+ 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
-    Get.put(AtomController());
-    Get.put(PageStateController());
+    final atomCtrl    = Get.put(AtomController());
+    final pageCtrl    = Get.put(PageStateController());
     Get.put(ChartControl());
     Get.put(CarouselProjectsControl());
-
+ 
+    // Jump to the deep-linked page once the first frame is rendered
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final int targetPage = _readInitialPage();
+      if (targetPage > 0) {
+        pageCtrl.jumpToPage(
+          pageItem: PageItem.values.firstWhere(
+            (p) => p.indexPage == targetPage,
+            orElse: () => PageItem.values.first,
+          ),
+          animate: false,
+        );
+      }
+    });
+ 
     return Builder(builder: (context) {
       return GetMaterialApp(
         translations: Dictionary(),
@@ -42,3 +71,4 @@ class MyApp extends StatelessWidget {
     });
   }
 }
+ 
